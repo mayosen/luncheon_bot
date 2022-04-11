07b5a812e5e236a2802bc31310a2f3716386b219
Yegor <gare577@gmail.com>
2022-04-11 11:48:46 +0300

    Add start command

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,6 +6,15 @@ from database.api import check_user
 from database.models import User
 
 
+def hello_user(update: Update, context: CallbackContext):
+    message = update.message
+    message.reply_text(
+        text=f"Привет, <b>{message.from_user.full_name}</b>!\n\n"
+             f"Я Ланч-бот, с моей помощью ты можешь заказать доставку еды на дом.\n"
+             f"Чтобы собрать новый заказ, используй команду /order",
+    )
+
+
 @check_user
 def get_me(update: Update, context: CallbackContext):
     user = User.get(id=update.message.from_user.id)
@@ -16,3 +25,4 @@ def get_me(update: Update, context: CallbackContext):
 
 def register(dp: Dispatcher):
     dp.add_handler(CommandHandler("me", get_me))
+    dp.add_handler(CommandHandler("start", hello_user))
