b7d83e38f4b0618acfbe7b10592727f8b34eb3c9
Yegor <gare577@gmail.com>
2022-04-23 11:14:10 +0300

    Add informing admins about exceptions

--- handlers/admins/errors.py
+++ handlers/admins/errors.py
@@ -1,27 +1,43 @@
+import logging
 import re
+import traceback
+from typing import Union
 
 from telegram import Update
 from telegram.ext import Dispatcher, CallbackContext, CommandHandler, CallbackQueryHandler
-from telegram.error import TelegramError
+from telegram.error import TelegramError, Unauthorized
 
 from database.api import get_admins, get_user
 from keyboards.admin import block_user, user_profile_keyboard
 from utils.formatting import format_user
 
 
-def unauthorized(update: object, context: CallbackContext):
+def error_dispatcher(update: Union[object, Update], context: CallbackContext):
     error = context.error
+    logging.error(msg="Exception while handling an update:", exc_info=context.error)
 
-    if not issubclass(error.__class__, TelegramError):
-        # TODO: Обработка Python исключений
+    if isinstance(error, Unauthorized):
+        unauthorized(update, context)
         return
 
-    if "mailing" not in context.bot_data:
-        # TODO: Обработка других Телеграм исключений
-        return
+    admins = get_admins()
+    trace = "<code>" + traceback.format_exc() + "</code>"
 
+    for admin in admins:
+        context.bot.send_message(
+            chat_id=admin.id,
+            text=f"Исключение при работе бота.\n\n"
+                 f"<b>{type(error).__name__}</b>: {error}\n"
+                 f"<b>args</b>: {error.args}\n"
+                 f"<b>user_data</b>: {context.user_data}\n"
+                 f"<b>bot_data</b>: {context.bot_data}\n\n"
+                 + trace,
+        )
+
+
+def unauthorized(update: Update, context: CallbackContext):
     bot = context.bot
-    user_id = context.bot_data["mailing"]["to_user"]
+    user_id = context.bot_data["mailing"]["to_user"] if "mailing" in context.bot_data else update.effective_user.id
     user = get_user(user_id=user_id)
     admins = get_admins()
 
@@ -61,7 +77,15 @@ def on_user_action(update: Update, context: CallbackContext):
         query.message.reply_text("Пользователь уже удален другим администратором.")
 
 
-def register(dp: Dispatcher):
-    dp.add_error_handler(unauthorized)
-    dp.add_handler(CallbackQueryHandler(pattern=r"^admin:user:(delete|pass):\d+$", callback=on_user_action))
+def test(update: Update, context: CallbackContext):
+    # TODO: Использовать специальную механику для рассылки
+    update.rrr()
 
+    # context.user_data["check"] = "me"
+    # update.message.reply_text("Test")
+
+
+def register(dp: Dispatcher):
+    dp.add_error_handler(error_dispatcher)
+    dp.add_handler(CallbackQueryHandler(pattern=r"^admin:user:(delete|pass):\d+$", callback=on_user_action))
+    dp.add_handler(CommandHandler("e", test))
